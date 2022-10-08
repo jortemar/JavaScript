422fb17b74957164811d70046941c342f5aeaeff
José Ortega <jortemar@gmail.com>
2022-10-08 05:47:19 +0200

    Tarea 2 subida

--- Ejercicios02/Ejercicio1.js
+++ Ejercicios02/Ejercicio1.js
@@ -1,17 +1,17 @@
-let provincias = ["Almería","Cádiz","Córdoba","Granada","Huelva","Jaén","Málaga","Sevilla"];
+let provincias = ["Granada","Cádiz","Sevilla","Almería","Córdoba","Málaga","Huelva","Jaén"];
 let salida = prompt("¿Cómo quiere que se muestre este ejercicio?\n" +
                     "1. Por pantalla\n" + 
                     "2. Por consola");
 
-let mensajeSalida = "El array tiene un tamaño de " + provincias.length + "\n";
-mensajeSalida += "Array original: " + provincias + "\n Array ordenado: " + provincias.sort() + "\n";
-mensajeSalida += "Array invertido: " + provincias.reverse() + "\n";
+let mensajeSalida = "El array tiene un tamaño de " + provincias.length + "<br>";
+mensajeSalida += "Array original: " + provincias + "<br> Array ordenado: " + provincias.sort() + "<br>";
+mensajeSalida += "Array invertido: " + provincias.reverse() + "<br>";
 let longitud = provincias.unshift("Moscú");
 longitud = provincias.push("Buenos Aires");
-mensajeSalida += "Array tras las nuevas entradas: " + provincias + "\n";
+mensajeSalida += "Array tras las nuevas entradas: " + provincias + "<br>";
 let elemento = provincias.pop();
 elemento = provincias.shift();
-mensajeSalida += "Array tras borrar las entradas anteriores " + provincias + "\n";
+mensajeSalida += "Array tras borrar las entradas anteriores: " + provincias + "<br>";
 
 if (provincias.includes("Madrid")) {
     let indice = provincias.indexOf("Madrid");
@@ -19,23 +19,48 @@ if (provincias.includes("Madrid")) {
 } else
     provincias.push("Madrid");
 
-mensajeSalida += "Array tras comprobar Madrid " + provincias + "\n";
-let cadena = provincias.toString();
+mensajeSalida += "Array tras comprobar Madrid: " + provincias + "<br>";
+let cadena = "Método toString: " + provincias.toString() + "<br>";
 
 if (salida == 1) {
     document.write(cadena);
 } else
     console.log(cadena);
 
-provincias.splice(provincias.indexOf("Córdoba"));
-mensajeSalida += "Array tras eliminar Córdoba " + provincias + "\n"; 
+provincias.splice(provincias.indexOf("Córdoba"),1);
+mensajeSalida += "Array tras eliminar Córdoba: " + provincias + "<br>"; 
 
+mensajeSalida += "Las tres últimas posiciones del array son: " + provincias.slice(provincias.length - 3) + "<br>";
 
+provincias.splice(2,3);
+mensajeSalida += "Array tras eliminar las posiciones 3ª,4ª y 5ª : " + provincias + "<br>";
 
 if (salida == 1) {
     document.write(mensajeSalida);
-} else
-    console.log(mensajeSalida);
+} else 
+    console.log(mensajeSalida); /*No funciona la salida por consola. Supongo que el mensaje será demasiado grande, así que 
+                                habría que ir metiendo las salidas una por una, o por grupos más pequeños.*/
+
+
+//Ejercicio 2
+
+let datos = [];
+let valor = prompt("Introduzca un valor");
+
+do {
+    datos.push(valor);
+    valor = prompt("Introduzca otro valor o la palabra 'fin' para terminar");
+
+} while (valor.toLowerCase() != "fin");
+
+provincias.push(datos);
+
+document.write("La suma de los dos arrays: " + provincias + "<br>");
+
+provincias.splice(provincias.indexOf(datos[0]),datos.length);
+document.write("El array de provincias vuelve a quedar así: " + provincias);
+
+
 
 
 
