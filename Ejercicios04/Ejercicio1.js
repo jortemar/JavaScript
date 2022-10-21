35a498268f5a835ea3e27c3999ea68aa64ec32c6
jortemar <jortemar@gmail.com>
2022-10-21 19:15:05 +0200

    21/10/22

--- Ejercicios04/Ejercicio1.js
+++ Ejercicios04/Ejercicio1.js
@@ -29,11 +29,86 @@ alert("Las tres primeras posiciones del array son " + numeros.slice(0,3));
 //apartado B
 if (numeros.lenght > 5) {
 
-}
+
     for (i in numeros) {
         if (i%2 == 0) {
-            
+            posicionesPares.push(numeros[i]);
         }
     }
+    alert ("Valores en posiciones pares");
+
+} else
+    alert("");
+
 
 //apartado C
+function cb(a) {
+    if (a % 2 == 0) {
+        return a;
+    } else {
+        return 1};
+}
+
+alert ("Producto de valores pares arrray " + productosPares);
+
+//apartadoD
+if (numeros.every((currentValue) => currentValue >= 0 && currentValue % 2 == 0)) {
+    let producto = numeros.reduce(function(a,b) {return a*b;},1);
+    alert("Producto de los valores del array: " + producto);
+
+} else  
+    alert("No todos los valores son pares o mayores que cero");
+
+//apartadoE
+if (numeros.some(a=>a%2==0 && a<10){
+    result=numeros.filter(a=>a%2==0 && a < 10);
+    let valor = result[0];
+    for (let i=valor; i <= 10; i ++) {
+        numeros.push(i);
+    }
+    alert(numeros);
+} else
+    alert("No existe un valor par menor de diez");
+
+
+//apartado seis
+numeros = numeros.reverse();
+alert(numeros);
+
+
+// apartado siete
+if (numeros.some(a => a ==10)) {
+    alert("El array contiene el número 10");
+    duplicado = numeros.slice();
+    alert("Muestro el duplicado: " + duplicado);
+} else
+    alert("El array no contiene el número 10");
+
+
+// apartado ocho
+duplicado = numeros.filter(a => a >= 0 );
+alert("El array contiene " + duplicado.lenght + " números positivos");
+
+// apartado nueve() {
+    let mensaje = "Contenido del Array: \n";
+    numeros.forEach(element => {
+        mensaje += element+ ",";
+    });
+    alert(mensaje);
+
+
+function inicializar() {
+    let arrayNumeros = [];
+    let arrayPares = [];
+    for (let i = 0; i < 20; i ++) {
+        numeros[i]= i + 1;
+        arrayNumeros[i] = Math.floor(Math.random()*100);
+        arrayPares[i] = Math.floor(Math.random() * 10)*2;
+
+    }
+    //function(a,b){return a - b;}
+    alert(arrayNumeros.sort(function(a,b){return a-b;}));
+    return(arrayNumeros.sort(function(a,b){return a-b;}));
+
+    //alert(arrayPares).sort...
+}
