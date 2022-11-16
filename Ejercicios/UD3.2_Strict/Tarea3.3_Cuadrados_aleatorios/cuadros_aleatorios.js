e512bdd7cadb1b2fba2c61acdd086eb72e64a1bd
jortemar <jortemar@gmail.com>
2022-11-16 09:57:51 +0100

    cuadros y color fondo terminado

--- Ejercicios/UD3.2_Strict/Tarea3.3_Cuadrados_aleatorios/cuadros_aleatorios.js
+++ Ejercicios/UD3.2_Strict/Tarea3.3_Cuadrados_aleatorios/cuadros_aleatorios.js
@@ -1,5 +1,7 @@
 function dibujarCuadrado(x,y,z,red,green,blue) {
-    document.write(`<div style = "background-color:rgb(${red}, ${green}, ${blue}); width:50px; height:50px; position:fixed; left:${x}vw;top:${y}vh;z-index:${z};"></div>`);
+    document.write(`<div style = "background-color:rgb(${red}, ${green}, ${blue});
+    width:50px; height:50px; position:fixed; left:${x}vw;top:${y}vh;z-index:${z};"></div>`);
+    //cada vez que se ejecuta la función, se inserta en el body del html una línea tal como esta
 }
 let posX,posY,posZ,rojo,verde,azul;
 
@@ -14,3 +16,15 @@ for (let i = 0; i < 2000; i++) {
 
     dibujarCuadrado(posX,posY,posZ,rojo,verde,azul);
 }
+
+//función opcional para reproducir una función indefinidamente con un intervalo determinado
+/*
+function cambiarColorFondo() {
+    let myInterval = null;
+    if (!myInterval) {
+        myInterval = setInterval(clonarCuadrado, 3000);
+    }
+}
+
+cambiarColorFondo();
+*/
