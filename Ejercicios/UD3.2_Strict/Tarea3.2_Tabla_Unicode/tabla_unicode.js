40a00c8b1a3ae2e3ee1e43ba50083b8e4c07ed17
jortemar <jortemar@gmail.com>
2022-11-19 04:10:43 +0100

    unicode terminado

--- Ejercicios/UD3.2_Strict/Tarea3.2_Tabla_Unicode/tabla_unicode.js
+++ Ejercicios/UD3.2_Strict/Tarea3.2_Tabla_Unicode/tabla_unicode.js
@@ -1,25 +1,32 @@
-
+document.write(`<h1 style = "text-align:center; padding-top:60px">Tabla UNICODE</h1>`);
 document.write(`<table style = "border:3px solid; border-collapse:collapse; 
-                width:70%; margin:100px auto" >`);
-let hexa, decimal;
+                width:70%; text-align:center; margin:100px auto" >`);
+
+let indice = 31;
 
 for (i = 0; i < 25; i++) {
-    document.write(`<tr></tr>`);
+    indice++;
+    document.write(`<tr>`);
 
-    for (j = 33; j <= 53; j++) {
-        document.write(`<td style = "border:1px solid"> ${convierteHexa(j)} </td>`);
+    for (j = indice; j < indice + 250; j = j + 25) {
+        document.write(`<td style = "border:1px solid; width:30px; background-color:cyan"><b> ${convierteHexa(j)} </b></td>`);
+        document.write(`<td style = "border:1px solid; width:20px"> ${convierteUnicode(j)} </td>`);
+        
     }
-    
+    document.write(`</tr>`);
 }
 
 document.write(`</table>`);
 
 
-function convierteHexa (numDecimal) {
-    //alert(`El número ${numDecimal} en hexadecimal es ${numDecimal.toString(16).toUpperCase()}`);
-    //el método .fromCharCode, que debe ir siempre con clase 'String' (no funciona con objeto String)
-    //nos convierte un número decimal en código unicode ('\u' + hexadecimal)
-    return String.fromCharCode(numDecimal);
+function convierteHexa(numDec) {
+    return numDec.toString(16).toUpperCase();
 }
 
-//alert(convierteHexa(33));
+function convierteUnicode(numDec) {
+    return String.fromCharCode(numDec);
+}
+
+//numDec en hexadecimal es numDec.toString(16)}`);
+//el método .fromCharCode(num en decimal), que debe ir siempre con clase 'String' (no funciona con objeto String)
+//nos convierte un número decimal en código unicode ('\u' + hexadecimal)
